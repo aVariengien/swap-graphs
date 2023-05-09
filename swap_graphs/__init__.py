22c55290a9d2d7e41a03833c369e6b0f81926ec6
Alexandre Variengien <alexandre.variengien@gmail.com>
2023-05-09 17:58:55 +0000

    public version

--- swap_graphs/__init__.py
+++ swap_graphs/__init__.py
@@ -4,4 +4,3 @@
 from swap_graphs.datasets.ioi import *  # type: ignore
 from swap_graphs.core import *  # type: ignore
 from swap_graphs.PatchedModel import *  # type: ignore
-from swap_graphs.datasets.nano_qa import *  # type: ignore
