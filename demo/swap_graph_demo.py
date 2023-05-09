cec1589328e1f7605743bf44813415b1b93fdab2
Alexandre Variengien <alexandre.variengien@gmail.com>
2023-05-09 18:22:47 +0000

    fixing init

--- demo/swap_graph_demo.py
+++ demo/swap_graph_demo.py
@@ -1,100 +1,25 @@
 # %%
 
-import copy
-import dataclasses
-import itertools
-import os
-import pickle
-import random
-import random as rd
-import time
-from copy import deepcopy
-from functools import partial
-from pathlib import Path
-from pprint import pprint
-from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
-import circuitsvis as cv
-import datasets
-import einops
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
+from functools import partial
+
 import plotly.express as px
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import tqdm.auto as tqdm
-import transformer_lens
-import transformer_lens.utils as utils
-from attrs import define, field
-
-from swap_graphs.datasets.ioi.ioi_dataset import NAMES_GENDER, IOIDataset
-from swap_graphs.datasets.ioi.ioi_utils import (
-    get_ioi_features_dict,
-    logit_diff,
-    logit_diff_comp,
-    probs,
-)
-from IPython import get_ipython  # type: ignore
-from jaxtyping import Float, Int
-from names_generator import generate_name
 from swap_graphs.core import (
-    ActivationStore,
     CompMetric,
     ModelComponent,
+    SgraphDataset,
     SwapGraph,
     WildPosition,
-    find_important_components,
-    SgraphDataset,
-    compute_clustering_metrics,
 )
+from swap_graphs.datasets.ioi.ioi_dataset import NAMES_GENDER, IOIDataset
+from swap_graphs.datasets.ioi.ioi_utils import get_ioi_features_dict
+from swap_graphs.utils import KL_div_sim
 from torch.utils.data import DataLoader
-from transformer_lens import (
-    ActivationCache,
-    FactoredMatrix,
-    HookedTransformer,
-    HookedTransformerConfig,
-)
-from transformer_lens.hook_points import (  # Hooking utilities
-    HookedRootModule,
-    HookPoint,
-)
-from transformer_lens.loading_from_pretrained import OFFICIAL_MODEL_NAMES
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
-from swap_graphs.utils import (
-    KL_div_sim,
-    L2_dist,
-    L2_dist_in_context,
-    imshow,
-    line,
-    plotHistLogLog,
-    print_gpu_mem,
-    save_object,
-    scatter,
-    show_attn,
-    get_components_at_position,
-    load_object,
-    show_mtx,
-    component_name_to_idx,
-)
-from swap_graphs.core import SgraphDataset, SwapGraph
-
-from tqdm import tqdm
-
-import networkx as nx
-from sklearn.metrics.cluster import adjusted_rand_score
+from transformer_lens import HookedTransformer
 
 torch.set_grad_enabled(False)
-
-import fire
-from transformer_lens import loading
-
-import plotly.express as px
-import igviz as ig
-
-import plotly
 
 
 # %% Load the model
--- swap_graphs/__init__.py
+++ swap_graphs/__init__.py
@@ -1,6 +1,7 @@
 # swap_graphs/__init__.py
 
 # import subpackages and submodules
-from swap_graphs.datasets.ioi import *  # type: ignore
-from swap_graphs.core import *  # type: ignore
-from swap_graphs.PatchedModel import *  # type: ignore
+
+from .core import *  # type: ignore
+from .PatchedModel import *  # type: ignore
+from .datasets.ioi import *  # type: ignore
